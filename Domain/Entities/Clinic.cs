c90aa8acd078ee5e2e5967c434a477bbd506bafd
Ramadan1011 <shukhratovich75@gmail.com>
2025-07-16 00:21:39 +0500

    add clinic and working hours entity

--- Domain/Entities/Clinic.cs
+++ Domain/Entities/Clinic.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace Domain.Entities
+namespace Domain.Entities;
+
+public class Clinic : AuditableEntity
 {
-    internal class Clinic
-    {
-    }
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = null!;
+
+    [MaxLength(1000)]
+    public string? Description { get; set; }
+
+    [Range(0, 5)]
+    public double Rating { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int Reviews { get; set; }
+
+    [MaxLength(300)]
+    public string? Address { get; set; }
+
+    [MaxLength(300)]
+    public string? Landmark { get; set; }
+
+    public virtual List<WorkingHours> WorkingHours { get; set; } = [];
+
+    [MaxLength(20)]
+    public string? Phone { get; set; }
+
+    [MaxLength(500)]
+    public string? Image { get; set; }
 }
---
+++ Domain/Entities/WorkingHours.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class WorkingHours : AuditableEntity
+{
+    public long ClinicId { get; set; }
+    public Clinic? Clinic { get; set; }
+
+    [Required]
+    public string DayOfWeek { get; set; } = default!;
+
+    [Required]
+    public string OpenTime { get; set; } = default!;
+
+    [Required]
+    public string CloseTime { get; set; } = default!;
+}
